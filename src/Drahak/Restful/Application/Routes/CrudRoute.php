006c98e7f15fb15a27b07dac531b2fe9c472b0ef
Drahomír Hanák <drahomir.hanak@gmail.com>
2013-07-29 00:10:46 +0200

    Allow to not set metadata in crud route

--- src/Drahak/Restful/Application/Routes/CrudRoute.php
+++ src/Drahak/Restful/Application/Routes/CrudRoute.php
@@ -15,7 +15,7 @@ class CrudRoute extends ResourceRoute
 	const ACTION_UPDATE = 'update';
     const ACTION_DELETE = 'delete';
 
-    public function __construct($mask, $metadata = '', $flags = IResourceRouter::CRUD)
+    public function __construct($mask, $metadata = array(), $flags = IResourceRouter::CRUD)
     {
         parent::__construct($mask, is_string($metadata) ? $metadata . ':default' : $metadata, $flags);
         $this->actionDictionary = array(
