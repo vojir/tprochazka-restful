39718fa9564bbf94c1af30f269b6893bb8f28e5e
Drahomír Hanák <drahomir.hanak@gmail.com>
2013-07-29 09:59:00 +0200

    Change default action for GET method to "read"

--- src/Drahak/Restful/Application/Routes/CrudRoute.php
+++ src/Drahak/Restful/Application/Routes/CrudRoute.php
@@ -11,7 +11,7 @@ class CrudRoute extends ResourceRoute
 
 	/** Presenter action names */
     const ACTION_CREATE = 'create';
-	const ACTION_READ = 'default';
+	const ACTION_READ = 'read';
 	const ACTION_UPDATE = 'update';
     const ACTION_DELETE = 'delete';
 
