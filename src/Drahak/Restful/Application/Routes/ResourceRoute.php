6ca3d4f796a79825a1da4f3a91d8a5980baec4e5
Robert Tupý <robert.tupy@economia.cz>
2016-01-14 18:14:16 +0100

    added method options to router and panel

--- src/Drahak/Restful/Application/Routes/ResourceRoute.php
+++ src/Drahak/Restful/Application/Routes/ResourceRoute.php
@@ -28,7 +28,8 @@ class ResourceRoute extends Route implements IResourceRouter
 		Http\IRequest::PUT => self::PUT,
 		Http\IRequest::HEAD => self::HEAD,
 		Http\IRequest::DELETE => self::DELETE,
-		'PATCH' => self::PATCH
+		'PATCH' => self::PATCH,
+		'OPTIONS' => self::OPTIONS,
 	);
 
 	/**
