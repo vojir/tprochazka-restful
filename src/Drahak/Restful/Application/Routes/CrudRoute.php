c04bd571f66ebeb3a61561fc59679a1c253e2b27
Adam Schubert <adam.schubert@sg1-game.net>
2017-01-10 20:44:26 +0100

    Fix issue when OPTIONS request was not working with CrudRoute

--- src/Drahak/Restful/Application/Routes/CrudRoute.php
+++ src/Drahak/Restful/Application/Routes/CrudRoute.php
@@ -31,13 +31,18 @@ class CrudRoute extends ResourceRoute
 			$metadata .= ':default';
 		}
         parent::__construct($mask, $metadata, $flags);
-		$this->actionDictionary = array(
+		$actions = array(
 			IResourceRouter::POST => self::ACTION_CREATE,
 			IResourceRouter::GET => self::ACTION_READ,
 			IResourceRouter::PUT => self::ACTION_UPDATE,
 			IResourceRouter::PATCH => self::ACTION_PATCH,
-			IResourceRouter::DELETE => self::ACTION_DELETE
+			IResourceRouter::DELETE => self::ACTION_DELETE,
 		);
+
+		foreach($actions AS $resource => $action)
+		{
+			$this->actionDictionary[$resource] = $action;
+		}
     }
 
 }
